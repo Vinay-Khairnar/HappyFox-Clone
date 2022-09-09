33c81e0d857c7f61b8806b8478116fef496c2264
AMahakud07 <abhishekmahakud007@gmail.com>
2022-09-09 09:38:49 +0530

    Home Page

--- homepage.js
+++ homepage.js
@@ -2,18 +2,23 @@ const tabs = document.querySelector(".wrapper");
 const tabButton = document.querySelectorAll(".tab-button");
 const contents = document.querySelectorAll(".content");
 
-tabs.onclick = e => {
+tabs.onclick = (e) => {
   const id = e.target.dataset.id;
   if (id) {
-    tabButton.forEach(btn => {
+    tabButton.forEach((btn) => {
       btn.classList.remove("active");
     });
     e.target.classList.add("active");
 
-    contents.forEach(content => {
+    contents.forEach((content) => {
       content.classList.remove("active");
     });
     const element = document.getElementById(id);
     element.classList.add("active");
   }
-}
+};
+
+const video = document.querySelector(".video-container");
+video.onclick = (e) => {
+  video.innerHTML = `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/cD0FfdHdNis" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+};
