29adb4b61c3c8c6f81c8765c3bf957693b1a779c
shivraj32644 <shivraj32644@gmail.com>
2022-09-10 16:12:29 +0530

    final submission

--- script/sorting.js
+++ script/sorting.js
@@ -1,30 +1,30 @@
-let sortFunction = () => {
-  let val = document.getElementById("sort").value;
-  let key = "id";
-  let ord = "asc";
+// let sortFunction = () => {
+//   let val = document.getElementById("sort").value;
+//   let key = "id";
+//   let ord = "asc";
 
-  if (val === "nto") {
-    key = "id";
-    ord = "asc";
-  } else if (val === "otn") {
-    key = "id";
-    ord = "desc";
-  } else if (val === "ltc") {
-    key = "priority";
-    ord = "asc";
-  } else if (val === "ctl") {
-    key = "priority";
-    ord = "desc";
-  }
+//   if (val === "nto") {
+//     key = "id";
+//     ord = "asc";
+//   } else if (val === "otn") {
+//     key = "id";
+//     ord = "desc";
+//   } else if (val === "ltc") {
+//     key = "priority";
+//     ord = "asc";
+//   } else if (val === "ctl") {
+//     key = "priority";
+//     ord = "desc";
+//   }
 
-  console.log(key, ord);
-  let url = `https://happyfox-clone-database.herokuapp.com/AllTickets?_sort=${key}&_order=${ord}`;
-  let check = async () => {
-    let response = await fetch(url);
-    let result = await response.json();
-    console.log(result);
-  };
-  check();
-};
+//   console.log(key, ord);
+//   let url = `https://happyfox-clone-database.herokuapp.com/AllTickets?_sort=${key}&_order=${ord}`;
+//   let check = async () => {
+//     let response = await fetch(url);
+//     let result = await response.json();
+//     console.log(result);
+//   };
+//   check();
+// };
 
-document.getElementById("sort").addEventListener("change", sortFunction);
+// document.getElementById("sort").addEventListener("change", sortFunction);
